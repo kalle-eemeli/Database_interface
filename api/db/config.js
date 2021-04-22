8067d66adf75f25d4c4881b15a0e628b1d9e5d3f
kalle-eemeli <60426643+kalle-eemeli@users.noreply.github.com>
2021-04-22 10:01:04 +0300

    Update config.js

--- api/db/config.js
+++ api/db/config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const config = {
-    host: "142.93.140.110",
+    host: "",
     port: "3310",
     user: "TTOW110",
-    password: "salasana",
+    password: "",
     database: process.env.ACTIVE_DB
 };
 
